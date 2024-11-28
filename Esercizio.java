be7ff3e108a8113d9916b9fb7d9aa9c3c5c348a8
FraXim <francesco.ximenes@marconibari.edu.it>
2024-11-28 13:38:12 +0000

    completato

--- Esercizio.java
+++ Esercizio.java
@@ -6,20 +6,42 @@ import java.util.Scanner;
 // Classe principale, con metodo main
 class Esercizio {
     // Il programma parte con una chiamata a main().
-    public static void main(String args[])
-    {
-        //Variabili del programma
-        String nome;
-
-        //Creo l'oggetto in per l'input da tastiera
-        Scanner in = new Scanner( System.in );
-
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
-
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+    public static void main(String args[]){
+        Scanner in =  new Scanner (System.in);
+        System.out.println("Inserisci quanti atleti ci partecipano alla gara");
+        int n=in.nextInt();
+        int i;
+        String[] nomi=new String[n];
+        int[] nazione=new int[n];
+        double[] tempo=new double[n];
+        for (i = 0; i<= n-1; i++){
+            System.out.println("Inserisci il nome del "+(i+1)+"Atleta: ");
+            nomi[i]=in.next();
+            System.out.println("Inserisci la nazione del "+(i+1)+"Atleta: ");
+            nazione[i]=in.nextInt();
+            System.out.println("Inserisci il tempo del "+(i+1)+"Atleta: ");
+            tempo[i]=in.nextDouble();
+        }
+        System.out.println("Lista + caratteristiche di ogni atleta: ");
+        for (i = 0; i<= n-1; i++){
+            System.out.println("Nome atleta "+(i+1)+": ");
+            System.out.println(nomi[i]);
+            System.out.println("Nazione atleta "+(i+1)+": ");
+            System.out.println(nazione[i]);
+            System.out.println("Tempo atleta "+(i+1)+": ");
+            System.out.println(tempo[i]);
+        }
+        double min=tempo[0];
+        int iMin=0;
+        for (i = 0; i<= n-1; i++){
+            if(tempo[i]< min){
+                iMin=i;
+                min=tempo[i];
+            }
+        }
+        System.out.println("Nome atleta vincente "+nomi[iMin]+": ");
+        System.out.println("Nazione atleta vincente "+nazione[iMin]+": ");
+        System.out.println("Tempo atleta vincente "+tempo[iMin]+": ");
     }
 }
 
